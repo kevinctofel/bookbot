525f5103ccdd898286d1ebb36d69f3ba1affa8e9
kevinctofel <kevin@kctofel.com>
2024-03-06 08:44:38 -0500

    added letter count to main()

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
   file_path = "books/frankenstein.txt"
   text = read_book(file_path)
   print (count_words(text))
+  print (count_letters(text))
   
 def read_book(path):
   with open(path) as f:
@@ -12,5 +13,14 @@ def count_words(text):
   words = text.split()
   # print(words)
   return len(words)
+
+def count_letters(text):
+
+  letters = {}
+  # key is a letter in string format
+  # value is an integer
+  for letter in text.lower():
+    letters[letter] = letters.get(letter, 0) + 1
+  print(letters)
 
 main()
