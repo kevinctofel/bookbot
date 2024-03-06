82171878881fb8db69921e2635a8b5c17c28b693
kevinctofel <kevin@kctofel.com>
2024-03-06 09:01:51 -0500

    Added report printing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
   file_path = "books/frankenstein.txt"
   text = read_book(file_path)
-  print (count_words(text))
-  print (count_letters(text))
+  total_words = count_words(text)
+  letter_dict = count_letters(text)
+  print_report(total_words, letter_dict)
   
 def read_book(path):
   with open(path) as f:
@@ -15,12 +16,23 @@ def count_words(text):
   return len(words)
 
 def count_letters(text):
-
   letters = {}
   # key is a letter in string format
   # value is an integer
   for letter in text.lower():
     letters[letter] = letters.get(letter, 0) + 1
-  print(letters)
+  return letters
+
+def print_report(words, letters):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f'\n{words} words found in the document\n')
+
+    for letter in alpha:
+      print(f"The '{letter}' character was found {letters.get(letter)} times")
+
+    print('\n--- End report ---')
+
 
 main()
